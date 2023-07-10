4edcb71d935728d7220e514240d805d378effb82
Vishnu <vishnu@propelinc.com>
2023-07-10 10:27:41 +0530

    implemented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route,RedirectFunction, useNavigate} from "react-router-dom";
 import Header from './component/Header/Header';
 import Home from './container/Home/Home';
-
+import Book from './container/Books/Books';
 function App() {
-
   return (
     <div className="App">
       <Header/>
-      <Home/>
+      <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path='/' element={<Home/>}/>
+          <Route path="/book" element={<Book />} />
+      </Routes>
+
     </div>
   );
 }
--- src/container/Home/Home.tsx
+++ src/container/Home/Home.tsx
@@ -8,6 +8,7 @@ title:string;
 }
 const Home: React.FC = () => {
   const [data, setData] = useState<DataItem[]>([]);
+  const [newdata,setNewData]=useState<DataItem[]>([]);
 
   useEffect(() => {
     fetchData();
@@ -18,24 +19,42 @@ const Home: React.FC = () => {
       const response = await fetch('https://serverlessnext-1-testvercel24.vercel.app/api/hello');
       const jsonData = await response.json();
       setData(jsonData);
+      const filter=data.filter((item)=>item.author==='Author 1');
+      setNewData(filter);
 
     } catch (error) {
       console.log('Error fetching data:', error);
     }
   };
   return(
-    <div className='all-books-container'>
-      <h6>All Books</h6>
-    <div className='book-container'>
-        {data.map((item) => (
-          <div className='book-card'>
-            <img src={item.image}/>
-            < h5>{item.title}</h5>
-            <p><b>Author:</b>{item.author}</p>
-            <p>{item.description}</p>
+    <div className='full-container'>
+      <div className='all-books-container'>
+        <h6>All Books</h6>
+          <div className='book-container'>
+            {data.map((item) => (
+              <div className='book-card'>
+                <img src={item.image}/>
+                < h5>{item.title}</h5>
+                <p><b>Author:</b>{item.author}</p>
+                <p>{item.description}</p>
+              </div>
+            ))}
           </div>
-      ))}
-    </div>
+        </div>
+        <div className='recent-books-sontainer'>
+          <h6>New Collection</h6>
+          <div className='book-container'>
+            {newdata.map((item)=>(
+              <div className='book-card'>
+                 <img src={item.image}/>
+                < h5>{item.title}</h5>
+                <p><b>Author:</b>{item.author}</p>
+                <p>{item.description}</p>
+                </div>
+            ))}
+
+          </div>
+        </div>
       </div>
   );
 }
